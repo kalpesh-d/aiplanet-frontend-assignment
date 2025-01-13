38c3a2110c3540c020431d6741e093fdb335468b
Kalpesh Dhoke <kalpeshdhoke7@gmail.com>
2025-01-13 16:24:36 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold underline">Hello world</div>
+      <img src="/openagi.svg" alt="Ai Planet" />
+    </>
+  )
+}
+
+export default App
